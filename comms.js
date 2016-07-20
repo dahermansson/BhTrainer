149d9832cbbd12e467fcac5f1924f55520842d54
Daniel Hermansson <daniel.hermansson@trafikverket.se>
2016-07-20 16:38:38 +0200

    init

---
+++ comms.js
@@ -0,0 +1,33 @@
+var comms = 
+[
+    {
+        'FirstTarget': 'S',
+        'SecondTarget': 'A',
+        'Case': "[L D' L', U']"
+    },
+    {
+        'FirstTarget': 'S',
+        'SecondTarget': 'C',
+        'Case': "[U, B D2 B']"
+    },
+    {
+        'FirstTarget': 'S',
+        'SecondTarget': 'D',
+        'Case': "[L D' L', U']"
+    },
+    {
+        'FirstTarget': 'S',
+        'SecondTarget': 'E',
+        'Case': "[L D' L', U']"
+    },
+    {
+        'FirstTarget': 'S',
+        'SecondTarget': 'F',
+        'Case': "[L D' L', U']"
+    },
+    {
+        'FirstTarget': 'S',
+        'SecondTarget': 'I',
+        'Case': "[L D' L', U']"
+    },
+];
