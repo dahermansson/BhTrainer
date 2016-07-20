87c1aaee785af7ef155163284f9419254ad39cae
Daniel Hermansson <daniel.hermansson@trafikverket.se>
2016-07-20 16:45:11 +0200

    fixade

--- comms.js
+++ comms.js
@@ -1,33 +1,33 @@
 var comms = 
 [
     {
-        'FirstTarget': 'S',
-        'SecondTarget': 'A',
-        'Case': "[L D' L', U']"
+        "FTarget": 	"S",
+        "STarget": 	"A",
+        "Case":		"[L D' L', U']"
     },
     {
-        'FirstTarget': 'S',
-        'SecondTarget': 'C',
-        'Case': "[U, B D2 B']"
+        "FTarget': 	"S",
+        "STarget': 	"C",
+        "Case': 	"[U, B D2 B']"
     },
     {
-        'FirstTarget': 'S',
-        'SecondTarget': 'D',
-        'Case': "[L D' L', U']"
+        "FTarget':	"S",
+        "STarget': 	"D",
+        "Case': 	"[U2, B D2 B']"
     },
     {
-        'FirstTarget': 'S',
-        'SecondTarget': 'E',
-        'Case': "[L D' L', U']"
+        "FTarget':	"S",
+        "STarget': 	"E",
+        "Case': 	"[L', B' R2 B]"
     },
     {
-        'FirstTarget': 'S',
-        'SecondTarget': 'F',
-        'Case': "[L D' L', U']"
+        "FTarget':	"S",
+        "STarget':	"F",
+        "Case': 	"F D' [U2, L2 D' L2 D L2] D F'"
     },
     {
-        'FirstTarget': 'S',
-        'SecondTarget': 'I',
-        'Case': "[L D' L', U']"
+        "FTarget':	"S",
+        "STarget':	"I",
+        "Case': 	"R' [U2, L' D' L] R"
     },
 ];
