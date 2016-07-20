0b563ce02e54f45e362ad49567c53ede1377794d
Daniel Hermansson <daniel.hermansson@trafikverket.se>
2016-07-20 16:55:44 +0200

    sista

--- comms.js
+++ comms.js
@@ -6,28 +6,88 @@ var comms =
         "Case":		"[L D' L', U']"
     },
     {
-        "FTarget': 	"S",
-        "STarget': 	"C",
-        "Case': 	"[U, B D2 B']"
+        "FTarget": 	"S",
+        "STarget": 	"C",
+        "Case": 	"[U, B D2 B']"
     },
     {
-        "FTarget':	"S",
-        "STarget': 	"D",
-        "Case': 	"[U2, B D2 B']"
+        "FTarget":	"S",
+        "STarget": 	"D",
+        "Case": 	"[U2, B D2 B']"
     },
     {
-        "FTarget':	"S",
-        "STarget': 	"E",
-        "Case': 	"[L', B' R2 B]"
+        "FTarget":	"S",
+        "STarget": 	"E",
+        "Case": 	"[L', B' R2 B]"
     },
     {
-        "FTarget':	"S",
-        "STarget':	"F",
-        "Case': 	"F D' [U2, L2 D' L2 D L2] D F'"
+        "FTarget":	"S",
+        "STarget":	"F",
+        "Case": 	"F D' [U2, L2 D' L2 D L2] D F'"
     },
     {
-        "FTarget':	"S",
-        "STarget':	"I",
-        "Case': 	"R' [U2, L' D' L] R"
+        "FTarget":	"S",
+        "STarget":	"I",
+        "Case": 	"R' [U2, L' D' L] R"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"J",
+        "Case": 	"[F L2 F', R2]"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"K",
+        "Case": 	"F [L2, F' R' F] F'"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"L",
+        "Case": 	"D2 [B, D F2 D'] D2"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"M",
+        "Case": 	"[F' U2 F, D]"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"N",
+        "Case": 	"F' [U2, R2 D' R2 D R2] F"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"O",
+        "Case": 	"[F' U2 F, D2]"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"P",
+        "Case": 	"[L, B' R2 B]"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"Q",
+        "Case": 	"[F' U2 F, D']"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"R",
+        "Case": 	"[R', D L' D']"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"U",
+        "Case": 	"[L2, B' R2 B]"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"V",
+        "Case": 	"U' [B R' B', L2] U"
+    },
+	{
+        "FTarget":	"S",
+        "STarget":	"W",
+        "Case": 	"D R' [L2 D' L2 D L2, U'] R D"
     },
 ];
