fb5fc5e166f53106be21c4147bff8aefce6582d4
Daniel <daniel.hermansson@gmail.com>
2016-07-21 21:28:43 +0200

    TX

--- comms.js
+++ comms.js
@@ -115,4 +115,134 @@ var comms =
         "STarget":	"F",
         "Case": 	"F' [D R' D', L2] F"
     },
+    {
+        "FTarget":	"T",
+        "STarget":	"I",
+        "Case": 	"D' R [B2, U' F U] R' D"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"J",
+        "Case": 	"[B, L F2 L']"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"K",
+        "Case": 	"D' [F2, R B' R'] D"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"L",
+        "Case": 	"[R' B2 R, F]"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"M",
+        "Case": 	"[R' B2 R, F']"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"N",
+        "Case": 	"[R U2 R', D]"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"O",
+        "Case": 	"[B', L F2 L']"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"P",
+        "Case": 	"[R U2 R', D2]"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"Q",
+        "Case": 	"R [U2, R2 D' R2 D R2] R'"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"R",
+        "Case": 	"[R U2 R', D]"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"U",
+        "Case": 	"D F [L2 D' L2 D L2, U] F' D'"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"V",
+        "Case": 	"U' [R U' R', D2] U"
+    },
+    {
+        "FTarget":	"T",
+        "STarget":	"W",
+        "Case": 	"[B2, L' F2 L]"
+    },
+    {
+        "FTarget":	"I",
+        "STarget":	"T",
+        "Case": 	"D' R [U' F U, B2] R' D"
+    },
+    {
+        "FTarget":	"J",
+        "STarget":	"T'",
+        "Case": 	"[L F2 L', B]"
+    },
+    {
+        "FTarget":	"K",
+        "STarget":	"T",
+        "Case": 	"D' [R B' R', F2] D"
+    },
+    {
+        "FTarget":	"L",
+        "STarget":	"T",
+        "Case": 	"[F, R' B2 R]"
+    },
+        {
+        "FTarget":	"M",
+        "STarget":	"T",
+        "Case": 	"[F, R' B2 R]"
+    },
+    {
+        "FTarget":	"N",
+        "STarget":	"T",
+        "Case": 	"[D, R U2 R']"
+    },
+    {
+        "FTarget":	"O",
+        "STarget":	"T",
+        "Case": 	"[L F2 L', B']"
+    },
+        {
+        "FTarget":	"P",
+        "STarget":	"T",
+        "Case": 	"[D2, R U2 R']"
+    },
+    {
+        "FTarget":	"Q",
+        "STarget":	"T",
+        "Case": 	"R [R2 D' R2 D R2, U2] R'"
+    },
+    {
+        "FTarget":	"R",
+        "STarget":	"T",
+        "Case": 	"[D, R U2 R']"
+    },
+        {
+        "FTarget":	"U",
+        "STarget":	"T",
+        "Case": 	"D F [U, L2 D' L2 D L2] F' D'"
+    },
+    {
+        "FTarget":	"V",
+        "STarget":	"T",
+        "Case": 	"U' [D2, R U' R'] U"
+    },
+    {
+        "FTarget":	"W",
+        "STarget":	"T",
+        "Case": 	"[L' F2 L, B2]"
+    },
 ];
